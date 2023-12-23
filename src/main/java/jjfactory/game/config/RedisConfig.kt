6a0584d11b3b66faf4905b1afa5221c5777a51e2
jaehyeong <wogud1514@gmail.com>
2023-12-23 21:57:03 +0900

    chore: data redis

---
+++ src/main/java/jjfactory/game/config/RedisConfig.kt
@@ -0,0 +1,26 @@
+package jjfactory.game.config
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+
+
+@Configuration
+class RedisConfig(
+    private val redisProperties: RedisProperties
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory? {
+        return LettuceConnectionFactory(redisProperties.host, redisProperties.port)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<*, *> {
+        val redisTemplate: RedisTemplate<*, *> = RedisTemplate<Any, Any>()
+        redisTemplate.connectionFactory = redisConnectionFactory()
+        return redisTemplate
+    }
+}
